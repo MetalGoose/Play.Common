311341adf1c3939a68284349e846f43ab08f7be7
Евгений <Freelans73@gmail.com>
2022-03-19 19:03:30 +0300

    Added overloaded methods for Get, GetAll

--- src/Play.Common/IRepository.cs
+++ src/Play.Common/IRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Play.Common
@@ -9,7 +10,9 @@ namespace Play.Common
         Task CreateAsync(T entity);
         Task DeleteAsync(Guid id);
         Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter);
         Task<T> GetAsync(Guid id);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
         Task UpdateAsync(T entity);
     }
 }
